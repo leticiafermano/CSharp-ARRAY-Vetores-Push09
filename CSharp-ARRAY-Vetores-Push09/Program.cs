a2a9ff7c8e6c4fd72601409690e16b5fe19cca65
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-17 20:07:02 -0300

    CSharp Course ARRAYS - Vetores

--- CSharp-ARRAY-Vetores-Push09/Program.cs
+++ CSharp-ARRAY-Vetores-Push09/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace CSharp_ARRAY_Vetores_Push09
+namespace LogicaDeProgramacao_P6_ARRAYS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             Vetores (ARRAYS)
+            É uma estrutura que permite armazenar informações de forma sequenciada.
+             */
+
+            string[] nomes = { "Letícia", "Célio", "Luana", "Vanda", "Vanderlei" };
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+
+            double[] valores = { 145, 10, 20, 50, 26 };
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.WriteLine(valores[i]);
+            }
         }
     }
 }
